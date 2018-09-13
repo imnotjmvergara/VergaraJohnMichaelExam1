bcc835d9f7011e310105ab4399c496e481d3f507
imnotjmvergara <2015081515@ust-ics.mygbiz.com>
2018-09-13 15:16:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jacobjm/vergarajohnmichaelexam1/VergaraJohnMichaelExam1.java
@@ -0,0 +1,13 @@
+package com.example.jacobjm.vergarajohnmichaelexam1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VergaraJohnMichaelExam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vergara_john_michael_exam1);
+    }
+}
